76f0e3cca6a80100177d11bc78b3cbabe6901162
Rich Chang <rich_chang@outlook.com>
2019-11-05 22:47:25 +0800

    Add game logic model and could player to the end
    -Add game status and active player var
    -Not yet to check if any player winning

--- app/src/main/java/com/richc/connect3/MainActivity.java
+++ app/src/main/java/com/richc/connect3/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +15,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    // 0 for yellow, 1 for red
+    int activePlayer = 0;
+
+    // To memory each grid status. 2 for unplayed. 0 or 1 means player.
+    int[] gameStatus = {2,2,2,2,2,2,2,2,2};
+
     public void dropIn (View view) {
         ImageView counter = (ImageView) view;
+        System.out.println("tag-"+counter.getTag().toString());
 
-        System.out.println(counter.getTag().toString());
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        counter.setTranslationY(-1000f);
-        counter.setImageResource(R.drawable.yellow);
-        counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+        // If grid is unplayed, then allow to toggle by player
+        if (gameStatus[tappedCounter] == 2) {
+            gameStatus[tappedCounter] = activePlayer;
+
+            counter.setTranslationY(-1000f);
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+        }
+        else {
+            Toast.makeText(MainActivity.this, "This grid already been played", Toast.LENGTH_SHORT).show();
+        }
     }
 }
